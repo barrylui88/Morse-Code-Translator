e54566afbdc9e866e509d3ed94038714828a6bbc
bcl <barrylui88@googlemail.com>
2023-02-10 19:02:29 +0000

    Linked JS variables with HTML

---
+++ assets/js/morse.js
@@ -0,0 +1,9 @@
+// Links to HTML Elements
+let morseInputTextbox = $("#morse-input-box");
+let letterOutputTextbox = $("#morse-input-box");
+let dotInputButton = $("#dot-input");
+let dashInputButton = $("#dash-input");
+let nextLetterButton = $("#next-letter-input");
+let nextWordButton = $("#next-word-input");
+let saveButton = $("#save-button");
+let clearButton = $("#clear-button");

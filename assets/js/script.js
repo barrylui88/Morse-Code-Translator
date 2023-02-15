e3938c80eef996cac8822b4703214b1531a4e9b9
bcl <barrylui88@googlemail.com>
2023-02-15 16:42:14 +0000

    Added Server API call for weather and translated morse output

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,41 @@ typewriter.typeString('Translate your message to letter')
     .pauseFor(2500)
     .deleteAll()
     .start();
+
+// Homepage Weather API Call
+
+// API URL Construction
+// TODAY SECTION
+let queryURLTop = "https://api.openweathermap.org/data/2.5/weather?q=";
+let queryTerm = "London"; // e.g. London
+let queryURLBottom = "&appid=";
+let APIKey = "1f171651aeaa44734ffe7198c60a1f2c";
+let responseCityName;
+let responseTodayTemperature;
+let responseTodayWind;
+let responseTodayHumidity;
+let homepageWeatherString;
+let homepageWeatherStringMorse;
+let weatherText = $("#weather");
+
+function callAPI () {
+    // Today URL
+    apiCall = $.ajax({
+        url: queryURLTop + queryTerm + queryURLBottom + APIKey,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        responseCityName = `${response["name"]}, ${response["sys"]["country"]}`;
+        responseTodayTemperature = response["main"]["temp"];
+        responseTodayWind = response["wind"]["speed"];
+        responseTodayHumidity = response["main"]["humidity"];
+        homepageWeatherString = `The temperature in ${responseCityName} is ${Math.floor(responseTodayTemperature-273.15)}°C with a ${Math.floor(responseTodayWind)}km/h wind speed and ${responseTodayHumidity}% humidity.`
+        homepageWeatherStringMorse = translationLettersToMorse(homepageWeatherString);
+        console.log(homepageWeatherString);
+        console.log(homepageWeatherStringMorse);
+        weatherText.html(homepageWeatherString);
+    })
+}
+callAPI();
+
+// Homepage Time API Call

d9d04c362ca4acfabcc9809aa6b47bea15697783
YueHuaHua <bulanarum@gmail.com>
2023-02-16 16:14:33 +0000

    added save button function

--- assets/js/morse.js
+++ assets/js/morse.js
@@ -8,11 +8,14 @@ let nextLetterButton = $("#next-letter-input");
 let nextWordButton = $("#next-word-input");
 let saveButton = $("#save-button");
 let clearButton = $("#clear-button");
+let historyButtons = $("#history-buttons")
 
 // JS Variables
 let currentMorseInputArray = [];
 let currentMorseInputString;
 let translatedLetterOutput;
+let history = [];
+
 
 // Event Listeners
 dotInputButton.click(function () {
@@ -46,14 +49,20 @@ nextWordButton.click(function () {
 })
 
 saveButton.click(function () {
-    // SAVE BUTTON FUNCTIONALITY
-    
+    console.log(`It's saved`);
+
+    let input = currentMorseInputString;
+    let output = translatedLetterOutput;
+    let historyItem = [input, output];
+    console.log(historyItem);
+    appendHistory(historyItem);
 })
 
 clearButton.click(function () {
     currentMorseInputArray = [];
     updateInputBox();
     updateOutputBox();
+    historyButtons.empty();
 })
 
 // JavaScript Functions
@@ -65,4 +74,38 @@ function updateInputBox () {
 function updateOutputBox () {
     translatedLetterOutput = translationMorseToLetters(currentMorseInputString);
     letterOutputTextbox.val(translatedLetterOutput);
+}
+
+function appendHistory(historyItem) {
+
+    console.log(history);
+    if(history.length >= 3){
+        history.shift();
+    }
+    
+    history.push(historyItem);
+    localStorage.setItem("translateHistory", JSON.stringify(history));
+    let stored = JSON.parse(localStorage.getItem("translateHistory"));
+    createButton(stored);
+}
+
+function createButton(stored) {
+    historyButtons.empty();
+
+    console.log("creating buttons");
+    for (let i = 0; i < stored.length; i++) {
+        
+        console.log("creating buttons II");
+        let button = document.createElement('button');
+        button.textContent = stored[i][0] + ` > ` + stored[i][1];
+
+        button.classList.add('btn');
+        button.classList.add('custom-button');
+        console.log("buttons created");
+        // buttons.addEventListener('click', function (event) {
+        // 	morseInputTextbox.val(stored[i][0]);
+        //     letterOutputTextbox.val(stored[i][1]);
+        // });
+        historyButtons.append(button);
+    }
 }
